ee33fdae81987505060bf263849b99fc67ffb47f
Canesa <javier.canesa2014@gmail.com>
2026-02-27 23:41:20 -0600

    answer
    closes #23

--- helloWorld/src/App.jsx
+++ helloWorld/src/App.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import ContactForm from "./components/forms/contactForm";
 import CallbackRenderDemo from "./components/CallbackRenderDemo";
+import UseMemoPerformanceDemo from "./components/UseMemoPerformanceDemo";
 
 function App() {
   const { t } = useTranslation();
@@ -9,6 +10,7 @@ function App() {
       <h1>{t("welcome")}</h1>
       <ContactForm />
       <CallbackRenderDemo />
+      <UseMemoPerformanceDemo />
     </main>
   );
 }
