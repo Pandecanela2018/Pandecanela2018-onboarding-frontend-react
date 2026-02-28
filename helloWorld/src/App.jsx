16e8cd593fcaf8857d0d112b072e445f6caa7d80
Canesa <javier.canesa2014@gmail.com>
2026-02-27 22:50:42 -0600

    answer
    Closes #22

--- helloWorld/src/App.jsx
+++ helloWorld/src/App.jsx
@@ -1,9 +1,16 @@
 import { useTranslation } from "react-i18next";
 import ContactForm from "./components/forms/contactForm";
+import CallbackRenderDemo from "./components/CallbackRenderDemo";
 
 function App() {
   const { t } = useTranslation();
-  return (<h1>{t("welcome")}</h1>)(<ContactForm></ContactForm>);
+  return (
+    <main>
+      <h1>{t("welcome")}</h1>
+      <ContactForm />
+      <CallbackRenderDemo />
+    </main>
+  );
 }
 
 export default App;
