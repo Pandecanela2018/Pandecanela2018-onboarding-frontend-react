c13986816572810202b0bc47ca75b266c740ceff
Canesa <javier.canesa2014@gmail.com>
2026-03-04 00:34:28 -0600

    answer
    closes #25

--- helloWorld/src/App.jsx
+++ helloWorld/src/App.jsx
@@ -1,18 +1,28 @@
 import { useTranslation } from "react-i18next";
+import { Link, Route, Routes } from "react-router-dom";
 import ContactForm from "./components/forms/contactForm";
 import CallbackRenderDemo from "./components/CallbackRenderDemo";
 import UseMemoPerformanceDemo from "./components/UseMemoPerformanceDemo";
 import UseEffectLifecycleDemo from "./components/UseEffectLifecycleDemo";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App() {
   const { t } = useTranslation();
   return (
     <main>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link>
+      </nav>
       <h1>{t("welcome")}</h1>
       <ContactForm />
       <CallbackRenderDemo />
       <UseMemoPerformanceDemo />
       <UseEffectLifecycleDemo />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </main>
   );
 }
