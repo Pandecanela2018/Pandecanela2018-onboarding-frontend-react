e81de8e7357a0911b6421bc665934effdca84ddc
Canesa <javier.canesa2014@gmail.com>
2026-03-06 15:55:55 -0600

    answer
    closes #27

--- helloWorld/src/components/Counter.jsx
+++ helloWorld/src/components/Counter.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../features/counter/counterSlice";
 import Button from "./Button";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <section className="mx-auto mt-6 w-full max-w-md rounded-xl border border-slate-200 bg-white p-6 shadow-sm">
       <h2 className="text-xl font-semibold text-slate-900">Counter</h2>
       <p className="mt-2 text-slate-700">Current count: {count}</p>
 
-      <div className="mt-4 flex justify-center gap-3">
-        <Button onClick={() => setCount((prev) => prev + 1)}>Increment</Button>
+      <div className="">
+        <Button onClick={() => dispatch(increment())}>Increment</Button>
+
         <Button
-          onClick={() => setCount((prev) => prev - 1)}
+          onClick={() => dispatch(decrement())}
           className="bg-slate-600 hover:bg-slate-700 active:bg-slate-800"
         >
           Decrement
